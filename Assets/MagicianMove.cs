c492528cea018aecf3988be5e940f8dc31914213
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-02-20 12:46:19 +0800

    Something useless

--- Assets/MagicianMove.cs
+++ Assets/MagicianMove.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicianMove : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +15,22 @@ public class MagicianMove : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(moveSpeed*Time.deltaTime, 0, 0);
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
         }
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-moveSpeed*Time.deltaTime, 0, 0);
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
         }
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, moveSpeed*Time.deltaTime, 0);
+            transform.Translate(0, moveSpeed * Time.deltaTime, 0);
         }
+
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -moveSpeed*Time.deltaTime, 0);
+            transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
         }
         // Time.deltaTime can avoid the frame rate problem
     }
